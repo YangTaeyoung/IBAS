37835f1c4f3536dc526024332cb01e9a001a29e0
YangTaeyoung <0130yang@gmail.com>
2021-02-17 01:10:15 +0900

    이름 변경 및 template 테스트 링크 추가

--- IBAS/urls.py
+++ IBAS/urls.py
@@ -15,10 +15,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-   #path('', include('first.urls')),
+    # path('', include('first.urls')),
     path('user/', include('member.urls')),
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('test/add/listing/', views.add_listing, name="add_listing"),
+    path('test/blog/detail/', views.blog_detail, name="blog_detail"),
+    path('test/blog/standard/', views.blog_standard, name="blog_standard"),
+    path('test/error/404/', views.error_404, name="error_404"),
+    path('test/contact/us/', views.contact_us, name="contact_us"),
+    path('test/coming/soon/', views.coming_soon, name="coming_soon"),
+    path('test/index/1/', views.index_1, name="coming_soon"),
+    path('test/index/2/', views.index_2, name="coming_soon"),
+    path('test/index/3/', views.index_3, name="coming_soon"),
+    path('test/listing/', views.listing, name="coming_soon"),
+    path('test/listing/details/1/', views.listing_details, name="coming_soon"),
+    path('test/listing/details/2/', views.listing_details_2, name="coming_soon"),
+    path('test/listing/details/3/', views.listing_details_3, name="coming_soon"),
+    path('test/listing/grid/left/sidebar/', views.listing_grid_left_sidebar, name="coming_soon"),
+    path('test/listing/grid/right/sidebar/', views.listing_grid_right_sidebar, name="coming_soon"),
+    path('test/listing/grid/map/left/sidebar/', views.listing_grid_map_left_sidebar, name="coming_soon"),
+    path('test/listing/grid/map/right/sidebar/', views.listing_grid_map_right_sidebar, name="coming_soon"),
+    path('test/listing/left/sidebar/', views.listing_left_sidebar, name="coming_soon"),
+    path('test/listing/right/sidebar/', views.listing_right_sidebar, name="coming_soon"),
+    path('test/register', views.register, name="coming_soon"),
 ]
---
+++ IBAS/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render
+
+
+def add_listing(request):
+    context = {}
+    return render(request, "add-listing.html", context)
+
+
+def blog_detail(request):
+    context = {}
+    return render(request, "blog-details.html", context)
+
+
+def blog_standard(request):
+    context = {}
+    return render(request, "blog-standard.html", context)
+
+
+def coming_soon(request):
+    context = {}
+    return render(request, "coming-soon.html", context)
+
+
+def contact_us(request):
+    context = {}
+    return render(request, "contact-us.html", context)
+
+
+def error_404(request):
+    context = {}
+    return render(request, "error-404.html", context)
+
+
+def index_1(request):
+    context = {}
+    return render(request, "index-1.html", context)
+
+
+def index_2(request):
+    context = {}
+    return render(request, "index-2.html", context)
+
+
+def index_3(request):
+    context = {}
+    return render(request, "index-3.html", context)
+
+
+def listing(request):
+    context = {}
+    return render(request, "listing.html", context)
+
+
+def listing_details(request):
+    context = {}
+    return render(request, "listing-details.html", context)
+
+
+def listing_details_2(request):
+    context = {}
+    return render(request, "listing-details-2.html", context)
+
+
+def listing_details_3(request):
+    context = {}
+    return render(request, "listing-details-3.html", context)
+
+
+def listing_grid_left_sidebar(request):
+    context = {}
+    return render(request, "listing-grid-left-sidebar.html", context)
+
+
+def listing_grid_map_left_sidebar(request):
+    context = {}
+    return render(request, "listing-grid-map-left-sidebar.html", context)
+
+
+def listing_grid_map_right_sidebar(request):
+    context = {}
+    return render(request, "listing-grid-map-right-sidebar.html", context)
+
+
+def listing_grid_right_sidebar(request):
+    context = {}
+    return render(request, "listing-grid-map-right-sidebar.html", context)
+
+
+def listing_left_sidebar(request):
+    context = {}
+    return render(request, "listing-left-sidebar.html", context)
+
+
+def listing_right_sidebar(request):
+    context = {}
+    return render(request, "listing-right-sidebar.html", context)
+
+
+def register(request):
+    context = {}
+    return render(request, "register.html", context)
