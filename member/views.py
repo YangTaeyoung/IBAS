7c5764381b3d18d49daab589cc8fee7e28830768
Yejin Yun <yyj11kr@naver.com>
2021-02-18 02:42:07 +0900

    회원가입(예진)

--- member/views.py
+++ member/views.py
@@ -118,4 +118,4 @@ def login(request):  # 로그인 페이지로 이동
     return render(request, 'login.html', {})
 
 def test_join(request):
-    return render(request, 'register_1.html', {})
+    return render(request, 'register_2.html', {})
