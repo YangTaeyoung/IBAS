27416421bd03b40a263c5bd2d682c76fdf283eba
JOYONGSIK <josik97@naver.com>
2021-02-19 17:50:43 +0900

    top_bar.html 수정

--- main/views.py
+++ main/views.py
@@ -10,5 +10,7 @@ def index(request): # 메인 홈페이지 단순 이동
 
 # 메인 작업중임.
 def index1(request):
-    context = {}
+
+    # login 은 세션이 있다고 가정한 것
+    context = {'login' : 1}
     return render(request, "top_bar.html", context)
