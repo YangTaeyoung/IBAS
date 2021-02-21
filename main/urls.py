125324ef742678419768a351c84c1a41887d1f20
JOYONGSIK <josik97@naver.com>
2021-02-21 14:27:27 +0900

    main.html 생성

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('main1/', views.index1, name='index1'),
+    path('main2/', views.index2, name='index2'),
     path('accounts/', include('allauth.urls')),
 ]
 
--- main/views.py
+++ main/views.py
@@ -8,9 +8,13 @@ def index(request): # 메인 홈페이지 단순 이동
     return render(request, 'index.html', {})
 
 
-# 메인 작업중임.
+# 탑바 작업중임.
 def index1(request):
 
     # login 은 세션이 있다고 가정한 것
     context = {'login' : 1}
     return render(request, "top_bar.html", context)
+
+def index2(request):
+    contest = {}
+    return render(request, 'main.html', contest)
