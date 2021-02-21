3a0559b8375ff6a5b7358140b333dc801e48d907
YangTaeyoung <apxkf9632@naver.com>
2021-02-21 19:57:06 +0900

    학번 중복체크 추가.

--- member/session.py
+++ member/session.py
@@ -12,12 +12,9 @@ def save_session(request, user):
     else: # 프로필 사진이 없을 경우
         request.session["user_pic"] = "0" # 0이 입력, 0일 경우 프로필 사진을 대체 이미지로 변경하면 될 듯.
     request.session["user_joined"] = str(user.user_joined)
-    print("날짜 제대로 출력 되나? ", str(user.user_joined))
     request.session["user_major"] = user.user_major.major_name
     request.session["user_phote"] = user.user_phone
     request.session["user_gen"] = user.user_gen
     request.session["user_grade"] = user.user_grade
-    print("왔음?")
     request.session["user_role"] = user.user_role.role_no
     request.session["user_auth"] = user.user_auth.auth_no
-    print("왔음?2")
--- member/views.py
+++ member/views.py
@@ -36,6 +36,7 @@ def join(request):  # 회원 가입 페이지로 이동 할 것인지, 이미 �
             tar_token.delete()
             tar_member.delete()
             auth_user.delete()
+            # ------------------------------소셜 로그인으로 받은 정보 처리 끝---------------------------------------#
 
             if len(User.objects.filter(
                     user_email=email)) == 0:  # 토큰 정보로 USER DB를 검색 했을 때 나오는 유저 정보가 없을 경우, 즉 입부 신청하지 않은 유저의 경
@@ -48,6 +49,7 @@ def join(request):  # 회원 가입 페이지로 이동 할 것인지, 이미 �
                     "email": email,
                     "name": name,
                     "pic": pic,
+                    "user_role": "5",
                     "stu_list": stu_list,
                     "quest_list": QuestForm.objects.all(),
                     "major_list": MajorInfo.objects.all(),
@@ -62,6 +64,75 @@ def join(request):  # 회원 가입 페이지로 이동 할 것인지, 이미 �
                 return redirect(reverse('index'))
     else:  # 파라미터가 제대로 넘어오지 않은 경우, 즉 비정상적인 경로를 통해 로그인 된 경우
         return render(request, "index.html", {'lgn_is_failed': 1})  # 자바 스크립트 경고를 띄우기 위한 변수 지정 후 index로 보냄.
+
+
+def choose_std_or_pro(request):  # 학생인지, 교수인지 고르게 하는 것.
+    if request.method == "POST":
+        if request.POST.get("password") is not None:  # pass페이지에서 password가 파라미터로 넘어왔을 경우에
+            user_token = request.POST.get("password")
+            if len(AuthUser.objects.filter(password=user_token)) == 0:  # 만약 넘어온 자료가 없으면
+                return redirect(reverse("index"))  # 홈으로 이동
+            auth_user = AuthUser.objects.filter(password=user_token)[0]  # auth테이블에서 해당 패스워드가 있는지 조회.
+
+            # 있다면 social account에서 앞서서 Auth의 primary key를 통해 가입한 친구의 pk를 넣어서 조회
+            tar_member = SocialAccount.objects.filter(user_id=auth_user.id)[0]  # quesyset의 첫번째 자료. 즉 로그인한 인원의 인스턴스 변수
+            tar_token = SocialToken.objects.filter(account_id=tar_member.id)[0]
+
+            # extra_data: 사용자의 동의를 통해 얻어온 권한인 듯.
+            email = tar_member.extra_data.get('email')  # 자동 완성을 위해 인스턴스 변수 설정
+            name = tar_member.extra_data.get('name')  # 자동 완성을 위한 이름 설정
+            pic = "0"  # 임시로 초기화
+            if tar_member.provider == "google":  # 사용자가 구글을 통해 로그인 한 경우
+                pic = tar_member.extra_data.get('picture')  # extra_data 테이블에서 꺼내는 변수를 picture로 설정
+            elif tar_member.provider == "naver":  # 사용자가 네이버를 통해 로그인 한 경우
+                pic = tar_member.extra_data.get('profile_image')  # extra_data 테이블에서 꺼내는 변수를 profile_image로 설정
+
+            # 소셜 로그인으로 부터 받은 정보는 저장하지 않기 위해 해당 정보 삭제
+            tar_token.delete()
+            tar_member.delete()
+            auth_user.delete()
+            # ------------------------------소셜 로그인으로 받은 정보 처리 끝---------------------------------------#
+
+            if len(User.objects.filter(
+                    user_email=email)) == 0:  # 토큰 정보로 USER DB를 검색 했을 때 나오는 유저 정보가 없을 경우, 즉 입부 신청하지 않은 유저의 경
+                # 컨텍스트에 자동완성 정보를 등록
+                stu_list = list()
+                for user in User.objects.all():
+                    stu_list.append(user.user_stu)
+
+                context = {
+                    "email": email,
+                    "name": name,
+                    "pic": pic,
+
+                }
+
+                return render(request, 'std_or_pro.html', context)
+            else:  # 이미 입부신청 되어있는 유저의 경우
+                # tar_member에 유저 정보를 저장
+                tar_member = User.objects.filter(user_email=email)[0]
+                # 로그인 및 정보 출력에 필요한 정보를 세션에 저장
+                session.save_session(request, tar_member)
+                return redirect(reverse('index'))
+    else:  # 파라미터가 제대로 넘어오지 않은 경우, 즉 비정상적인 경로를 통해 로그인 된 경우
+        return render(request, "index.html", {'lgn_is_failed': 1})  # 자바 스크립트 경고를 띄우기 위한 변수 지정 후 index로 보냄.
+
+
+def join_1(request):
+    stu_list = list()
+    for user in User.objects.all():
+        stu_list.append(user.user_stu)
+
+    context = {
+        "email": request.POST.get("email"),
+        "name": request.POST.get("name"),
+        "pic": request.POST.get("pic"),
+        "user_role": request.POST.get("user_role"),
+        "stu_list": stu_list,
+        "quest_list": QuestForm.objects.all(),
+        "major_list": MajorInfo.objects.all()
+    }
+    return render(request, "join.html", context)
 
 
 def join_chk(request):  # 회원 가입 페이지로 부터 정보를 받아 가입 처리를 하는 메서드
@@ -113,22 +184,27 @@ def quest_chk(request):
             user_phone=user_phone,  # 핸드폰 번호
             user_pic=user_pic  # 프로필 사진
         )
-        # 모든 질문 리스트를 뽑아옴: 질문이 몇번까지 있는지 알아야 답변을 몇번까지 했는지 알기 때문
-        quest_list = QuestForm.objects.all()
-
         # 사용자 정보를 DB에 저장
         user.save()
-        # 질문에 대한 답변을 저장하는 곳
-        for quest in quest_list:
-            answer = Answer.objects.create(
-                answer_quest=quest,
-                answer_cont=request.POST.get("answer_" + str(quest.quest_no)),
-                answer_user=user,
-            )
-            answer.save()
+
+        if user_role == "5":  # 오직 일반 학생으로 가입했을 때만
+            # 모든 질문 리스트를 뽑아옴: 질문이 몇번까지 있는지 알아야 답변을 몇번까지 했는지 알기 때문
+            quest_list = QuestForm.objects.all()
+
+            # 질문에 대한 답변을 저장하는 곳
+
+            for quest in quest_list:
+                answer = Answer.objects.create(
+                    answer_quest=quest,
+                    answer_cont=request.POST.get("answer_" + str(quest.quest_no)),
+                    answer_user=user,
+                )
+
+                answer.save()
+
         session.save_session(request, user)
         return redirect(reverse("index"))
-    return render(request,"index.html", {'lgn_is_failed': 1})
+    return render(request, "index.html", {'lgn_is_failed': 1})
 
 
 def pass_param(request):  # 구글 로그인으로 부터 파라미터를 받아 넘기는 페이지, 사용자에겐 보이지 않음.
@@ -150,6 +226,7 @@ def test_join1(request):
 
 def test_join2(request):
     return render(request, 'register_2-2.html', {})
-def std_or_pro(request):
-    return render(request,'std_or_pro.html', {})
 
+
+def std_or_pro(request):
+    return render(request, 'std_or_pro.html', {})
