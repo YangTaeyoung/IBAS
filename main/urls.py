cc20b62801dc0e4456107c31bf6f54550eb5effe
JOYONGSIK <josik97@naver.com>
2021-02-22 21:05:29 +0900

    이름 변경 및 수정

--- main/urls.py
+++ main/urls.py
@@ -3,12 +3,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('main1/', views.index1, name='index1'), # 탑바 작업
-    path('main2/', views.index2, name='index2'), # 메인 작업
-    path('main3/', views.index3, name='index3'), # 동아리 소개 작업
-    path('main4/', views.index4, name='index4'), # 하단 바 작업
-    path('test/activity', views.test_activity, name='index3'), # 동아리 소개 작업
+    path('main', views.main, name="main"), #메인
+    path('main/top_bar/', views.top_bar, name='top_bar'), # 탑바 작업
+    path('main/bottom_bar', views.bottom_bar, name='bottom_bar'), # 하단 바 작업
+    path('main/test_activity/', views.test_activity, name='test_activity'), # 동아리 소개 작업
     path('accounts/', include('allauth.urls')),
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,34 +1,34 @@
 from django.shortcuts import render, get_object_or_404
 from allauth.socialaccount.models import SocialAccount  # 소셜 계정 DB, socialaccount_socialaccount 테이블을 사용하기 위함.
-from DB.models import AuthUser, User, ChiefCarrier  # 전체 계정 DB, AuthUser 테이블을 사용하기 위함.
+from DB.models import AuthUser, User, ChiefCarrier, UserRole  # 전체 계정 DB, AuthUser 테이블을 사용하기 위함.
 from member import session
 
 # Create your views here.
 
-def index(request): # 메인 홈페이지 단순 이동
-    return render(request, 'index.html', {})
+# def index(request): # 메인 홈페이지 단순 이동
+#     return render(request, 'index.html', {})
 
 
 # 탑바 작업
-def index1(request):
+def top_bar(request):
     # 세션은 세션이 있다고 가정한 것
     session.save_session(request,User.objects.get(pk='12162359'))
     context = {}
     return render(request, "top_bar.html", context)
 
 # 메인 작업
-def index2(request):
+def main(request):
     contest = {}
     return render(request, 'main.html', contest)
 
 # 동아리 소개 작업할 것임
 def test_activity(request):
-    chief = ChiefCarrier.objects.filter(user_role=1).select_related()
-    sub_chief = ChiefCarrier.objects.filter(user_role=2).select_related()
-    context = {}
-    return render(request, 'activity.html', context)
+    chief = get_object_or_404(User, user_role=1)
+    sub_chief = get_object_or_404(User, user_role=2)
+    context = {'chief' : chief , 'sub_chief' : sub_chief}
+    return render(request, 'introduce.html', context)
 
-def index4(request):
+def bottom_bar(request):
     chief = User.objects.filter(user_role=get_object_or_404(UserRole, role_no=1))[0]
     session.save_chief(request, chief)
     return render(request, 'bottom_bar.html', {})
