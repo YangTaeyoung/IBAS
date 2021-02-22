0ecd92e4422934f4c86838a19ee0be5010368b3c
JOYONGSIK <josik97@naver.com>
2021-02-22 21:07:15 +0900

    이름 변경 및 수정 2

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('main', views.main, name="main"), #메인
+    path('main/', views.main, name="main"), #메인
     path('main/top_bar/', views.top_bar, name='top_bar'), # 탑바 작업
     path('main/bottom_bar', views.bottom_bar, name='bottom_bar'), # 하단 바 작업
     path('main/test_activity/', views.test_activity, name='test_activity'), # 동아리 소개 작업
