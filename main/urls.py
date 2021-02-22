30734fc3dd8618aa11474eb184ec521bc9ac350e
JOYONGSIK <josik97@naver.com>
2021-02-22 21:17:46 +0900

    이름 변경 및 수정 3

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('test/', views.test_main, name="main"), #메인
     path('test/top_bar/', views.test_top_bar, name='top_bar'), # 탑바 작업
-    path('test/botton_bar/', views.test_bottom_bar, name='bottom_bar'), # 하단 바 작업
+    path('test/bottom_bar/', views.test_bottom_bar, name='test_bottom_bar'), # 하단 바 작업
     path('test/activity/', views.test_activity, name='test_activity'), # 동아리 소개 작업
     path('test/accounts/', include('allauth.urls')),
 ]
