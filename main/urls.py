1f47a8f8dc7dc6891a2b38a32b7eb68b81231096
JOYONGSIK <josik97@naver.com>
2021-02-22 16:20:13 +0900

    회장단 소개 만들고있는 중임

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('main1/', views.index1, name='index1'),
-    path('main2/', views.index2, name='index2'),
+    path('main1/', views.index1, name='index1'), # 탑바 작업
+    path('main2/', views.index2, name='index2'), # 메인 작업
+    path('main3/', views.index3, name='index3'), # 동아리 소개 작업
     path('accounts/', include('allauth.urls')),
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from allauth.socialaccount.models import SocialAccount  # 소셜 계정 DB, socialaccount_socialaccount 테이블을 사용하기 위함.
-from DB.models import AuthUser, User  # 전체 계정 DB, AuthUser 테이블을 사용하기 위함.
+from DB.models import AuthUser, User, ChiefCarrier  # 전체 계정 DB, AuthUser 테이블을 사용하기 위함.
 from member import session
 
 # Create your views here.
@@ -9,14 +9,21 @@ def index(request): # 메인 홈페이지 단순 이동
     return render(request, 'index.html', {})
 
 
-# 탑바 작업중임.
+# 탑바 작업
 def index1(request):
-    # login 은 세션이 있다고 가정한 것
+    # 세션은 세션이 있다고 가정한 것
     session.save_session(request,User.objects.get(pk='12162359'))
     context = {}
     return render(request, "top_bar.html", context)
 
-# 메인 작업중임
+# 메인 작업
 def index2(request):
     contest = {}
     return render(request, 'main.html', contest)
+
+# 동아리 소개 작업할 것임
+def index3(request):
+    chief = ChiefCarrier.objects.filter(user_role=1).select_related()
+    sub_chief = ChiefCarrier.objects.filter(user_role=2).select_related()
+    contest = {}
+    return render(request, 'activaty.html', contest)
