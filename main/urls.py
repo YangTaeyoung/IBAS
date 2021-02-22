8ea9cf3059bbc1d1bc8a77400602dd99188b0756
JOYONGSIK <josik97@naver.com>
2021-02-23 01:47:00 +0900

    중간 커밋 엔 푸쉬

--- main/urls.py
+++ main/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('test/introduce/', views.test_introduce, name='test_introduce'), # 동아리 소개 작업
     path('test/test_activity/', views.test_activity, name='test_activity'), # 동아리 활동 게시판
     path('test/accounts/', include('allauth.urls')),
+    path('test/test_activity/detail/', views.test_activity_detail, name='test_activity_detail'), # 동아리 활동 자세히 보기
+    path('test/test_activity/register/', views.test_activity_register, name='test_activity_register'), # 동아리 등록하기
 ]
 
