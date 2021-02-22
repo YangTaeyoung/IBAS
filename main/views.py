aac57d45742a18c67961f34ec04f1e7315066c72
JOYONGSIK <josik97@naver.com>
2021-02-22 15:24:12 +0900

    session

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from allauth.socialaccount.models import SocialAccount  # 소셜 계정 DB, socialaccount_socialaccount 테이블을 사용하기 위함.
-from DB.models import AuthUser  # 전체 계정 DB, AuthUser 테이블을 사용하기 위함.
+from DB.models import AuthUser, User  # 전체 계정 DB, AuthUser 테이블을 사용하기 위함.
+from member import session
 
 # Create your views here.
 
@@ -12,7 +13,8 @@ def index(request): # 메인 홈페이지 단순 이동
 def index1(request):
 
     # login 은 세션이 있다고 가정한 것
-    context = {'login' : 1}
+    session.save_session(request,User.objects.get(pk='12162359'))
+    context = {}
     return render(request, "top_bar.html", context)
 
 def index2(request):
