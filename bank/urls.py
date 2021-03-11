33eea43afacf9acfdfffda8ffaa70789b923c18f
Yejin0O0 <78341274+Yejin0O0@users.noreply.github.com>
2021-03-11 18:02:12 +0900

    진행중

---
+++ bank/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from . import views
+
+# Media files 저장 및 전달 설정하기 위한 import
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+
+    path('list/', views.bank_board, name='bank_board'),  # 회계 게시판 작업
+
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
