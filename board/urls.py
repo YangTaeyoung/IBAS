99cc2aded7790ae5e59492b853dc8a9a3911abc5
cocacolalover777 <tmddus970613@naver.com>
2021-03-12 15:54:19 +0900

    상세 게시글 완성.

--- board/urls.py
+++ board/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('list/', views.board, name="board"), #게시판페이지로 이동
+    path('detail/', views.detail, name="detail") #게시판 상세게시판으로 이동
 ]
--- board/views.py
+++ board/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def board(request):  # 게시판 페이지로 이동
     return render(request, 'board.html', {})
+
+def detail(request):
+    return render(request, 'detail.html', {})
