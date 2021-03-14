5a2ec11ae694c1824a1e3301c92f7e6353d66675
YangTaeyoung <0130yang@gmail.com>
2021-03-15 04:02:43 +0900

    유저 정보 가져오는 함수 재조정.

--- IBAS/user_controller.py
+++ IBAS/user_controller.py
@@ -1,26 +1,44 @@
-from DB.models import User
+from DB.models import User, ChiefCarrier,UserRole
 
 
 def is_chief_exist():  # 회장 정보가 있는지 체크
-    return len(User.objects.filter(user_role=1)) != 0
+    return len(User.objects.filter(user_role=UserRole.objects.get(pk=1))) != 0
 
 
 def is_sub_chief_exist():  # 부회장 정보가 있는지 체크
-    return len(User.objects.filter(user_role=2)) != 0
+    return len(User.objects.filter(user_role=UserRole.objects.get(pk=2))) != 0
+
+
+def is_banker_exist():
+    return len(User.objects.filter(user_role=UserRole.objects.get(pk=4))) != 0
+
+
+def is_chief_crews_exist():
+    return len(User.objects.filter(user_role=UserRole.objects.get(pk=3))) != 0
 
 
 def get_chief():  # 회장 정보를 가져옴
     if is_chief_exist():
-        return User.objects.filter(user_role=1)[0]
+        return User.objects.filter(user_role=UserRole.objects.get(pk=1))[0]
     else:
         return None
 
 
 def get_sub_chief():  # 부회장 정보를 가져옴
     if is_sub_chief_exist():
-        return User.objects.filter(user_role=2)[0]
+        return User.objects.filter(user_role=UserRole.objects.get(pk=2))[0]
     else:
         return None
+
+
+def get_banker():
+    if is_banker_exist():
+        return User.objects.filter(user_role=UserRole.objects.get(pk=4))[0]
+
+
+def get_chief_crews():
+    if is_chief_crews_exist():
+        return User.objects.filter(user_role=UserRole.objects.get(pk=3))
 
 
 def get_logined_user(request):  # 로그인한 유저의 정보를 가져옴
