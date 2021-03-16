9573f1cd0bce0a3febb5c37e21248fda3e8884a1
YangTaeyoung <apxkf9632@naver.com>
2021-03-17 02:12:35 +0900

    대댓글 관련 함수 수정.

--- main/static/js/sy.js
+++ main/static/js/sy.js
@@ -1,13 +1,13 @@
 // 대댓글창 삽입
-function addReply() {
+function addReply(comment_id, url, board_no, csrf_token) {
     // 답글달기 버튼 안보이게
-    const btnAdd = document.getElementById('btnAdd');
+    const btnAdd = document.getElementById('btnAdd-' + comment_id);
     btnAdd.style.display = 'none';
 
     // 대댓글 입력창에 추가되는 모든걸 담는 div를 생성
     const reply_box = document.createElement('div');
     reply_box.classList.add("btnRemove");
-    document.getElementById('comment_no_1').appendChild(reply_box);
+
 
     // 대댓글 입력창 윗쪽 구분선
     const line = document.createElement('div');
@@ -16,8 +16,24 @@ function addReply() {
 
     // 대댓글 입력 form 태그
     const form = document.createElement('form');
+    form.innerHTML = csrf_token;
     form.classList.add('comment-form');
+    form.id = "comment-ref-register-" + comment_id;
+    form.action = url;
+    form.method = "post";
     reply_box.appendChild(form);
+
+    const inputCommentId = document.createElement("input");
+    inputCommentId.type = "hidden";
+    inputCommentId.name = "comment_ref"
+    inputCommentId.value = comment_id;
+    form.appendChild(inputCommentId);
+
+    const inputBoardNo = document.createElement("input");
+    inputBoardNo.type = "hidden";
+    inputBoardNo.name = "board_no";
+    inputBoardNo.value = board_no;
+    form.appendChild(inputBoardNo);
 
     // 대댓글 창을 꾸미는 p태그
     const p = document.createElement('p');
@@ -28,6 +44,7 @@ function addReply() {
     const textarea = document.createElement('textarea');
     textarea.setAttribute('placeholder', '답글을 입력하세요!');
     textarea.setAttribute('row', '8');
+    textarea.setAttribute("name", "comment_cont");
     p.appendChild(textarea);
 
     // 대댓글 작성취소 및 답글작성을 담는 버튼 div
@@ -54,8 +71,15 @@ function addReply() {
     btn2.setAttribute('type', 'submit');
     btn2.classList.add('site-button', 'radius-xl', 'm-l10', 'm-r15');
     btn2.innerText = "답글작성"
+    btn2.addEventListener("click", event=>submitRef(comment_id));
     btndiv.appendChild(btn2);
 
+    document.getElementById('comment_no_' + comment_id).appendChild(reply_box);
+}
+
+function  submitRef(comment_id)
+{
+    document.getElementById("form-ref-register"+comment_id).submit();
 }
 
 // // 댓글수정 (DB 적용 ver)
@@ -92,14 +116,14 @@ function addReply() {
 // 댓글수정 (DB 적용X - 테스트용 ver)
 // 위에 주석처리된 것을 참고하여 수정
 // 수정필요한 부분1
-function correcting_comment() {
+function comment_update(comment_id) {
     // 수정필요한 부분2
-    const reply_comment = document.getElementById('correcting-no-1');
+    const reply_comment = document.getElementById('correcting-no-' + comment_id);
     reply_comment.disabled = false;
     reply_comment.setAttribute('class', 'happy');
 
     // 댓글수정 버튼 안보이게 하기
-    const correctingBtn = document.getElementById('correctingBtn');
+    const correctingBtn = document.getElementById('correctingBtn-' + comment_id);
     correctingBtn.style.display = 'none';
 
     // 댓글수정완료 버튼 생성
@@ -110,21 +134,25 @@ function correcting_comment() {
     checkicon.innerText = " 수정완료";
     correctingresultBtn.appendChild(checkicon);
     // 수정완료 버튼 타입을 submit으로 지정
-    correctingresultBtn.setAttribute('type', 'submit');
+    correctingresultBtn.setAttribute('type', 'button');
+    correctingresultBtn.addEventListener("click", event => submit_update(comment_id))
     // 수정필요한 부분3
-    document.getElementById('comment_no_1').appendChild(correctingresultBtn);
-
+    document.getElementById('form-comment-delete-' + comment_id).appendChild(correctingresultBtn);
     // 마우스포커스 맨 뒤로 가게 하기
     var len = $(reply_comment).val().length;
     $(reply_comment).focus();
     $(reply_comment)[0].setSelectionRange(len, len);
 }
 
+function submit_update(comment_id) {
+    document.getElementById("form-comment-update-" + comment_id).submit();
+}
 
 // 댓글삭제
 function comment_del(comment_id) {
-    if (confirm('정말로 삭제하시겠습니까?')) {
-        document.getElementById("comment-delete-" + comment_id).submit()
+    if (confirm('정말로 삭제하시겠습니까?')) // 삭제 할 지 묻고 동의 시
+    {
+        document.getElementById("form-comment-delete-" + comment_id).submit() // 삭제 폼을 전송
     }
 }
 
