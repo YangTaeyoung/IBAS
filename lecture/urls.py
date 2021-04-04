3de98bc66526d195dd6243e5679c3c6a19fffba4
CHAERIM <cofla030360@gmail.com>
2021-04-04 23:21:54 +0900

    04.02 공모전 게시판 상세, 등록

--- lecture/urls.py
+++ lecture/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('contest/list', views.contest_list, name="contest_list") # 공모전 게시판페이지로 이동
+    path('contest/list', views.contest_list, name="contest_list"),# 공모전 게시판페이지로 이동
+    path('contest/register', views.contest_register, name="contest_register"), # 공모전 게시판 등록페이지로 이동
+    path('contest/detail', views.contest_detail, name="contest_detail") # 공모전 게시판 상세보기페이지로 이동
+
 ]
--- lecture/views.py
+++ lecture/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def contest_list(request):  # 게시판 페이지로 이동
     return render(request, 'contest_board.html', {})
+
+def contest_register(request):  # 게시판 페이지로 이동
+    return render(request, 'contest_register.html', {})
+
+def contest_detail(request):  # 게시판 페이지로 이동
+    return render(request, 'contest_detail.html', {})
