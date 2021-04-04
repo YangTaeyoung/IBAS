296c0f06a98b0a6d328e9fad3d4c46d79dbf02b3
Taeyoung Yang <0130yang@gmail.com>
2021-04-04 19:37:10 +0900

    내가 쓴 게시글 리스트 정렬 기준 변경.

--- my_info/views.py
+++ my_info/views.py
@@ -8,7 +8,7 @@ import os
 def my_info(request):
     if request.session.get("user_stu") is not None:
         my_info = User.objects.get(pk=request.session.get("user_stu"))
-        my_board_list = Board.objects.filter(board_writer=my_info).order_by("board_type_no").order_by("-board_created")
+        my_board_list = Board.objects.filter(board_writer=my_info).order_by("board_type_no__board_type_no").order_by("-board_created")
         my_comment_list = Comment.objects.filter(comment_writer=my_info).order_by(
             "comment_board_no__board_type_no").order_by("-comment_created")
         my_bank_list = Bank.objects.filter(bank_used_user=my_info).order_by("-bank_used")
