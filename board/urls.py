c5af362c4335332d5fe43f90b839aacb69ce3286
CHAERIM <cofla030360@gmail.com>
2021-04-05 00:25:39 +0900

    04.02 공모전 board로 이동

--- board/urls.py
+++ board/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('list/', views.board, name="board"), #게시판페이지로 이동
-    path('detail/', views.detail, name="detail") #게시판 상세게시판으로 이동
+    path('detail/', views.detail, name="detail"), #게시판 상세게시판으로 이동
+    path('contest/list', views.contest_list, name="contest_list"),  # 공모전 게시판페이지로 이동
+    path('contest/register', views.contest_register, name="contest_register"),  # 공모전 게시판 등록페이지로 이동
+    path('contest/detail', views.contest_detail, name="contest_detail")  # 공모전 게시판 상세보기페이지로 이동
 ]
--- board/views.py
+++ board/views.py
@@ -5,3 +5,12 @@ def board(request):  # 게시판 페이지로 이동
 
 def detail(request):
     return render(request, 'detail.html', {})
+
+def contest_list(request):  # 게시판 페이지로 이동
+    return render(request, 'contest_board.html', {})
+
+def contest_register(request):  # 게시판 등록 페이지로 이동
+    return render(request, 'contest_register.html', {})
+
+def contest_detail(request):  # 게시판 상세 페이지로 이동
+    return render(request, 'contest_detail.html', {})
