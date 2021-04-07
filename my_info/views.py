3eb1ded0b097c3370c9b5f4ff04a647bdf488f12
Yang Taeyoung <0130yang@gmail.com>
2021-04-07 14:59:53 +0900

    회원 관리 수정 완료.

--- my_info/views.py
+++ my_info/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse
-from DB.models import Board, User, Comment, Lect, LectBoard, BoardType, UserRole, Bank,UserAuth
+from DB.models import Board, User, Comment, Lect, LectBoard, BoardType, UserRole, Bank, UserAuth
 from django.db.models import Q
 from django.core.paginator import Paginator
 from pagination_handler import get_paginator_list
@@ -16,8 +16,8 @@ def my_info(request):
         new_user_list = User.objects.filter(user_auth__auth_no=3)  # 신입 부원 리스트
         exist_user_list = User.objects.filter(~Q(user_auth__auth_no=3))  # 기존 회원 리스트
         new_user_items = get_paginator_list(request, "new_user", new_user_list, 10)
-        exist_user_items = get_paginator_list(request, "exist_user", exist_user_list, 10)
-        auth_list = UserAuth.objects.all()
+        exist_user_items = get_paginator_list(request, "exist_user", exist_user_list, 2)
+        auth_list = UserAuth.objects.filter(auth_no__lte=2)  # 기존 회원은 미승인 회원으로 넘길 수 없으므로, role_no 가 2 이하인 튜플만 가져옴.
         role_list = UserRole.objects.all()
         context = {
             "my_board_list": my_board_list,
--- pagination_handler.py
+++ pagination_handler.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
 
 
+# 1. request, 2. "페이지의 이름" 3. 페이지네이션 하고 싶은 쿼리셋 리스트 4. 페이지 네이션 숫자. 몇 개씩 한 페이지에 보이게 할 것인지.
 def get_paginator_list(request, name, pg_list, num):
     paginator = Paginator(pg_list, num)  # 페이지네이터로 10개씩 보이게끔. 나중에 수정하면 됌
     page = request.GET.get(name + '_page')  # 페이지 이름 ㅇㅇ 여기서 변경하면 됌
