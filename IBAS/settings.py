4020d284038f01b18593ce63b16107bdc7057f76
Yang Taeyoung <0130yang@gmail.com>
2021-04-07 20:13:45 +0900

    세팅 수정.

--- IBAS/settings.py
+++ IBAS/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'IBAS',
         'USER': 'root',
-        'PASSWORD': 'webproj1793--',
+        'PASSWORD': 'webproj3971--',
         'HOST': 'localhost',
         'PORT': '3356',
     }
