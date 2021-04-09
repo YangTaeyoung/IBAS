9bb7677215292d5814e701bc91af21e9f8907574
Yang Taeyoung <0130yang@gmail.com>
2021-04-10 01:23:02 +0900

    어떤 함수에서든 context를 등록하기 위한 수단. context_processor.

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,6 +1,18 @@
 from DB.models import Alarm, User
+from django.db.models import Q
 from user_controller import get_logined_user
+from date_controller import today, today_before_day, today_after_day
 
 
 def alarms(request):
-    return {"alarm_list": Alarm.objects.filter(alarm_user=User.objects.get(pk=get_logined_user(request)))}
+    my_alarm = Alarm.objects.filter(
+        Q(alarm_user=get_logined_user(request)) & Q(
+            alarm_date__range=[today_before_day(3), today_after_day(0)])).order_by("alarm_ischecked").order_by(
+        "-alarm_date")
+
+    return {
+        "alarm_list": my_alarm
+    }
+
+def chief(request):
+    return {"chief": User.objects.filter(user_role__role_no=1).first()}
