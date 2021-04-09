d401e6bcde00f33e4d4d675aa921382707272ba7
Yang Taeyoung <0130yang@gmail.com>
2021-04-10 01:23:27 +0900

    날짜범위를 조회하기 위한 컨트롤러.

--- date_controller.py
+++ date_controller.py
@@ -1,6 +1,16 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.utils.dateformat import DateFormat
 
 
 def today():
     return str(DateFormat(datetime.now()).format("Y-m-d h:i:s"))
+
+
+def today_after_day(day):
+    after = datetime.now() + timedelta(days=day)
+    return after
+
+
+def today_before_day(day):
+    before = datetime.now() - timedelta(days=day)
+    return before
