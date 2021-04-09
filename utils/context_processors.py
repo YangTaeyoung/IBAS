b7d79ff1b5ef6f2182b05f402a0c211cd91084cc
Yang Taeyoung <0130yang@gmail.com>
2021-04-10 01:37:23 +0900

    로그인 안했을 때의 알람 오류 수정.

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -5,14 +5,17 @@ from date_controller import today, today_before_day, today_after_day
 
 
 def alarms(request):
-    my_alarm = Alarm.objects.filter(
-        Q(alarm_user=get_logined_user(request)) & Q(
-            alarm_date__range=[today_before_day(3), today_after_day(0)])).order_by("alarm_ischecked").order_by(
-        "-alarm_date")
+    if get_logined_user(request) is not None:
+        my_alarm = Alarm.objects.filter(
+            Q(alarm_user=get_logined_user(request)) & Q(
+                alarm_date__range=[today_before_day(3), today_after_day(0)])).order_by("alarm_ischecked").order_by(
+            "-alarm_date")
 
-    return {
-        "alarm_list": my_alarm
-    }
+        return {
+            "alarm_list": my_alarm
+        }
+    else:
+        return None;
 
 def chief(request):
     return {"chief": User.objects.filter(user_role__role_no=1).first()}
