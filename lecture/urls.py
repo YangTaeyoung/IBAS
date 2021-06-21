40c7a00bd3960e50b42d179f28b883bb1a45741a
CHAERIM <cofla030360@gmail.com>
2021-06-21 15:03:51 +0900

    채림 수정사항 적용

--- lecture/urls.py
+++ lecture/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('detail/<int:lect_no>', views.lect_detail, name="lect_detail"),  # lecture 게시판 상세보기페이지로 이동
     path('<int:type_no>/view/', views.lect_view, name="lect_view"),  # 공모전 게시판페이지로 이동
     path('room/main/', views.lectRoom_main, name="lectRoom_main"), # 강의룸 메인 게시판으로 이동
-    path('room/register/', views.lect_room_register, name="lect_room_register"), # 강의룸 게시글 등록페이지로 이동
+    path('board/register/', views.lect_board_register, name="lect_board_register"), # 강의룸 게시글 등록페이지로 이동
 ]
