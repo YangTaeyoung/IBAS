80ccad7137984f5bb4d3401077b0fdc43275f048
Yang Taeyoung <0130yang@gmail.com>
2021-06-25 19:11:55 +0900

    (6.25)유투브 iframe 태그를 얻어오는 함수 작성.

--- utils/youtube.py
+++ utils/youtube.py
@@ -1,3 +1,6 @@
+# 내부 호출용 함수
+# 유투브 URL뒷부분을 넣으면 iframe 태그를 반환해줌
+# 사용시에는 autoescape off를 사용하고 쓸 것.
 def get_youtube_tag(behind_of_url):
     return  '<iframe width="560" height="315" ' \
             'src="https://www.youtube.com/embed/' + behind_of_url + '"' \
@@ -12,6 +15,8 @@ def get_youtube_tag(behind_of_url):
             '</iframe>'
 
 
+# 유투브 url을 넣으면 iframe 태그를 반환하는 함수
+# 사용시에는 autoescape off를 사용하고 쓸 것.
 def get_youtube(url: str):
     equal_idx = url.rfind('=')
     slash_idx = url.rfind('/')
