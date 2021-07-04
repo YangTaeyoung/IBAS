200ba41e46d444ad986fd296b28a803dd37eeae5
DongHyeon <ydh9516@gmail.com>
2021-07-04 15:49:01 +0900

    (유동현) testCase 추가 : 강의자 출석 및 과제 페이지 상단 회차별 강의 제목

--- lecture/tests.py
+++ lecture/tests.py
@@ -1,3 +1,21 @@
+from django.http import HttpRequest
 from django.test import TestCase
+from DB.models import Lect
+from lecture.views import lect_room_attend_teacher
+import lecture
 
-# Create your tests here.
+
+class LectBoardTest(TestCase):
+    def test_for_getting_all_lectBoards_in_a_lecture_room(self):
+        """
+                강의자 메뉴 中 : 출석 및 과제 페이지에서 상단 회차별 강의 제목
+
+        """
+        lect_room = Lect.objects.first()
+        lect_board_title_list = [lecture.lect_title for lecture in lect_room.lectures]
+
+        request = HttpRequest()
+        response = lect_room_attend_teacher(request, lect_room.lect_no)
+
+        for lect_title in lect_board_title_list:
+            self.assertIn(lect_title, response.content.decode())
