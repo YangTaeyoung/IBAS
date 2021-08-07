3515ded6df6986cf64ddbdc5f8f935a3b5108c9f
DongHyeonYu <ydh9516@gmail.com>
2021-08-07 19:28:33 +0900

    (유동현) 활동게시판 / 공모전 게시판 / 게시판 / 강의게시판 / 강의과제게시판 유효성 검사 js 적용

--- static/js/validation_check.js
+++ static/js/validation_check.js
@@ -1,24 +1,4 @@
 // common
-function validation_check_for_file_upload() {
-    let error = '이미지 파일을 적어도 한개 이상 등록해주세요!\n';
-
-    $("div.imageuploadify-details").each(function () {
-        let file_type = $(this).children('span:eq(1)').text()
-        if (file_type.includes("image")) {
-            error = null;
-            return false;
-        }
-    })
-
-    let uploaded_image_files = $("img.file-img")
-    if (uploaded_image_files != null && uploaded_image_files.length > 0) {
-        return null;
-    }
-
-    return error;
-}
-
-// common
 function validation_check_for_title() {
     let error = null; // if there are no any errors, error=null, or error =  '...'
 
@@ -39,15 +19,30 @@ function validation_check_for_contents() {
 
     // semmernote 가 생성한 iframe 태그 밑의 document 에 접근.
     let content_div = $('div.summernote-div').children().contents().find('div.note-editable');
+    if(content_div.text().trim().length > 0) {
+        error = null;
+    }
 
-    $.each($(content_div).children('p'), function() {
-        if($(this).text().trim().length > 0) {
+    return error;
+}
+
+function validation_check_for_img_file_upload() {
+    let error = '이미지 파일을 적어도 한개 이상 등록해주세요!\n';
+
+    $("div.imageuploadify-details").each(function () {
+        let file_type = $(this).children('span:eq(1)').text()
+        if (file_type.includes("image")) {
             error = null;
             return false;
         }
-    });
+    })
 
-    return error
+    let uploaded_image_files = $("img.file-img")
+    if (uploaded_image_files != null && uploaded_image_files.length > 0) {
+        return null;
+    }
+
+    return error;
 }
 
 function  validation_check_for_common_things() {
@@ -55,7 +50,6 @@ function  validation_check_for_common_things() {
     let array_of_validation_method = [
         validation_check_for_title,
         validation_check_for_contents,
-        validation_check_for_file_upload,
     ]
 
     let errors = [];
@@ -109,7 +103,13 @@ function validation_check_for_contest() {
         contest_deadline = new Date(contest_deadline);
         if (contest_start >= contest_deadline) {
         errors.push('공모전 마감날짜는 시작날짜보다 뒤여야 합니다!\n');
+        }
     }
+
+    // validate whether some image files exist or not
+    let img_file_error = validation_check_for_img_file_upload()
+    if (img_file_error.length > 0) {
+        errors.push(img_file_error);
     }
 
 
@@ -120,6 +120,23 @@ function validation_check_for_contest() {
     } else {
         return true;
     }
+}
 
+// for board consist of only title and content, files are not necessary
+function validation_check_for_board(is_activity=false) {
+    let errors = validation_check_for_common_things();
+
+    // 활동 게시판은 사진 필수
+    if (is_activity) {
+        errors.push(validation_check_for_img_file_upload());
+    }
+    
+    // if there are any errors, not submit contest_form and alert all messages!
+    if (errors.length > 0) {
+        alert(errors.join(''));
+        return false;
+    } else {
+        return true;
+    }
 }
 
