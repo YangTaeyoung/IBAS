c45beb59a8a78a4743000509f0a5d4ca11156472
Yang Taeyoung <0130yang@gmail.com>
2021-08-13 06:16:40 +0900

    8.13 알람이 미확인 된 것부터 뜨도록 설정.

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -12,8 +12,8 @@ def alarms(request):
     if is_logined(request):
         my_alarm = Alarm.objects.filter(
             Q(alarm_user=get_logined_user(request)) & Q(
-                alarm_date__range=[today_before_day(3), today_after_day(0)])).order_by("alarm_ischecked").order_by(
-            "-alarm_date")
+                alarm_date__range=[today_before_day(3), today_after_day(0)])).order_by("-alarm_date").order_by(
+            "alarm_ischecked")
         return {
             "alarm_list": my_alarm,
             "alarm_len": len(my_alarm.filter(~Q(alarm_ischecked=1)))
