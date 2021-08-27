a063655cecfbbd0e1b4b4d49d25fb011ac93fda1
DongHyeon <ydh9516@gmail.com>
2021-08-27 20:15:38 +0900

    (유동현) 서버 실행 settings 변경

--- wsgi.py
+++ wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IBAS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IBAS.prod.settings')
 
 application = get_wsgi_application()
